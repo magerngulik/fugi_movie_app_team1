41ae947e95ddb2772d0fbe97298cd1ef9cc102be
Deny <flutterlabz@gmail.com>
2022-08-16 16:24:50 +0700

    .

---
+++ lib/shared/util/input/input.dart
@@ -0,0 +1,24 @@
+import '../input_control/input_control.dart';
+
+class Input {
+  static Map<String, dynamic> values = {};
+  static Map<String, InputControlState> inputController = {};
+
+  static set(key, value) {
+    values[key] = value;
+  }
+
+  static get(key) {
+    return values[key];
+  }
+
+  static clear(key) {
+    values.remove(key);
+  }
+
+  static resetValues() {
+    inputController.forEach((key, value) {
+      value.resetValue();
+    });
+  }
+}
---
+++ lib/shared/widget/textfield/textarea.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:fugi_movie_app/shared/widget/textfield/textfield.dart';
+
+class ExTextArea extends StatelessWidget {
+  final String id;
+  final String label;
+  final int maxLines;
+  final String? value;
+
+  const ExTextArea({
+    Key? key,
+    required this.id,
+    required this.label,
+    this.maxLines = 4,
+    this.value,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ExTextField(
+      id: id,
+      label: label,
+      maxLines: 4,
+      value: value,
+    );
+  }
+}
