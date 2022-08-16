2a79b2226cf7866abe3db8044ed0a3d5340b493f
Deny <flutterlabz@gmail.com>
2022-08-16 16:27:01 +0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:fugi_movie_app/firebase_options.dart';
 import 'package:fugi_movie_app/module/admin/admin_main_navigation/view/admin_main_navigation_view.dart';
+import 'package:fugi_movie_app/shared/util/theme/theme.dart';
 import 'package:get/get.dart';
 
 void main() async {
@@ -15,6 +16,7 @@ void main() async {
       debugShowCheckedModeBanner: false,
       defaultTransition: Transition.fadeIn,
       transitionDuration: const Duration(milliseconds: 900),
+      theme: defaultTheme,
       home: AdminMainNavigationView(),
     ),
   );
