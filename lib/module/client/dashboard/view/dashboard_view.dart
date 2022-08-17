bb30904bfd1f1118076463b327239c3768ad70a3
Deny <flutterlabz@gmail.com>
2022-08-17 18:03:18 +0700

    .

--- lib/module/client/dashboard/view/dashboard_view.dart
+++ lib/module/client/dashboard/view/dashboard_view.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import '../controller/dashboard_controller.dart';
 
@@ -14,6 +15,49 @@ class DashboardView extends StatelessWidget {
         return Scaffold(
           appBar: AppBar(
             title: Text("Dashboard"),
+          ),
+          body: Column(
+            children: [
+              SizedBox(
+                height: 140.0,
+                child: StreamBuilder<QuerySnapshot>(
+                  stream: FirebaseFirestore.instance
+                      .collection("movies")
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasError) return Text("Error");
+                    if (!snapshot.hasData) return Text("No Data");
+                    final data = snapshot.data!;
+
+                    return ListView.builder(
+                      itemCount: data.docs.length,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        var item = (data.docs[index].data() as Map);
+
+                        return Container(
+                          width: 200.0,
+                          child: Row(
+                            children: [
+                              Image.network(
+                                "https://i.ibb.co/S32HNjD/no-image.jpg",
+                                width: 60.0,
+                              ),
+                              const SizedBox(
+                                width: 10.0,
+                              ),
+                              Expanded(
+                                child: Text(item["original_title"] ?? ""),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
         );
       },
