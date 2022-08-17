aa42de34b6b19202c23f5b98e05ba1bb4ac7b448
rsyd29 <brasyid15@gmail.com>
2022-08-17 22:27:34 +0700

    Create TabBar ['Now Showing', 'Coming Soon'] view on Menu Dashboard

--- lib/module/client/dashboard/view/dashboard_view.dart
+++ lib/module/client/dashboard/view/dashboard_view.dart
@@ -13,78 +13,107 @@ class DashboardView extends StatelessWidget {
       builder: (controller) {
         controller.view = this;
 
-        return Scaffold(
-          backgroundColor: Colors.white,
-          body: Column(
-            children: [
-              SizedBox(
-                height: 140.0,
-                child: StreamBuilder<QuerySnapshot>(
-                  stream: FirebaseFirestore.instance
-                      .collection("movies")
-                      .snapshots(),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasError) return Text("Error");
-                    if (!snapshot.hasData) return Text("No Data");
-                    final data = snapshot.data!;
+        return SafeArea(
+          child: Scaffold(
+            backgroundColor: Colors.white,
+            body: StreamBuilder<QuerySnapshot>(
+              stream:
+                  FirebaseFirestore.instance.collection("movies").snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) return Text("Error");
+                if (!snapshot.hasData) return Text("No Data");
+                // final data = snapshot.data!;
 
-                    return ListView.builder(
-                      itemCount: data.docs.length,
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (context, index) {
-                        var item = (data.docs[index].data() as Map);
+                // MovieModel itemMovie = MovieModel.fromJson(item);
 
-                        return Container(
-                          width: 200.0,
-                          child: Row(
-                            children: [
-                              Image.network(
-                                "https://i.ibb.co/S32HNjD/no-image.jpg",
-                                width: 60.0,
-                              ),
-                              const SizedBox(
-                                width: 10.0,
-                              ),
-                              Expanded(
-                                child: Text(item["original_title"] ?? ""),
-                              ),
-                            ],
-                          ),
-                        );
-                      },
+                return Stack(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          stops: [
+                            Get.height * 0.00001,
+                            Get.height * 0.0005,
+                          ],
+                          colors: const [
+                            Color(0xff58534b),
+                            Colors.white,
+                          ],
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 32.0,
+                      left: 0,
+                      right: 0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: controller.tabBar.map<Widget>(
+                          (e) {
+                            int index = controller.tabBar.indexOf(e);
+                            return Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 8.0),
+                              child: Obx(() {
+                                return OutlinedButton(
+                                  style: OutlinedButton.styleFrom(
+                                    backgroundColor:
+                                        controller.selectedTabBar.value == index
+                                            ? Color(0xfffe5422)
+                                            : Colors.transparent,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(16),
+                                    ),
+                                  ),
+                                  onPressed: () =>
+                                      controller.changeTabBar(index),
+                                  child: Text(
+                                    e,
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                );
+                              }),
+                            );
+                          },
+                        ).toList(),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+            bottomNavigationBar: BottomAppBar(
+              color: Colors.white,
+              elevation: 0,
+              child: SizedBox(
+                height: Get.height * 0.1,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                  child: Obx(() {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children:
+                          controller.allMenuBottomNavigationBar.map<Widget>(
+                        (e) {
+                          int index =
+                              controller.allMenuBottomNavigationBar.indexOf(e);
+                          return BottomNavigationBarWidget(
+                            text: e,
+                            tooltip: e,
+                            isSelected:
+                                controller.selectedBottomNavigationBar.value ==
+                                    index,
+                            onPressed: () => controller
+                                .changeIndexBottomNavigationBar(index),
+                          );
+                        },
+                      ).toList(),
                     );
-                  },
+                  }),
                 ),
-              ),
-            ],
-          ),
-          bottomNavigationBar: BottomAppBar(
-            color: Colors.white,
-            elevation: 0,
-            child: SizedBox(
-              height: Get.height * 0.1,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 4.0),
-                child: Obx(() {
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: controller.allMenuBottomNavigationBar.map<Widget>(
-                      (e) {
-                        int index =
-                            controller.allMenuBottomNavigationBar.indexOf(e);
-                        return BottomNavigationBarWidget(
-                          text: e,
-                          tooltip: e,
-                          isSelected:
-                              controller.selectedBottomNavigationBar.value ==
-                                  index,
-                          onPressed: () =>
-                              controller.changeIndexBottomNavigationBar(index),
-                        );
-                      },
-                    ).toList(),
-                  );
-                }),
               ),
             ),
           ),
