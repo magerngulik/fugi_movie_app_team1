d5f35eef9a7d661f95d4d57dd4a00d47c5edc97d
Deny <flutterlabz@gmail.com>
2022-09-01 09:43:02 +0700

    .

--- lib/module/client/movie_video_player/controller/movie_video_player_controller.dart
+++ lib/module/client/movie_video_player/controller/movie_video_player_controller.dart
@@ -9,7 +9,8 @@ class MovieVideoPlayerController extends GetxController {
   int index = 0;
   String asset =
       // "https://github.com/denyocrworld/test_upload_video/raw/main/video.mp4";
-      "https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4";
+      // "https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4";
+      "https://magerngulik.my.id/pasarmoderen/DC%20LEAGUE%20OF%20SUPER-PETS%20%E2%80%93%20Official%20Trailer.mp4";
 
   bool display = false;
   bool lock = true;
